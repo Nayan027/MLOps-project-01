d60aa323d16517e018e898307690de0f582d62f9
Nayan Parvez <nayanparvez90@gmail.com>
2025-03-04 05:45:09 -0800

    set-up file necessary code executed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nayan Parvez",
+    author_email="nayanparvez90@gmail.com",
+    packages=find_packages()
+)
