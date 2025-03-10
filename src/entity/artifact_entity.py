b8dbd2f7e032d80bfdd71de82798db95dcbb7b22
Nayan Parvez <nayanparvez90@gmail.com>
2025-03-10 13:42:32 -0700

    ARTIFACT-entity added for Model Pusher

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -43,3 +43,11 @@ class ModelEvaluationArtifact:
     changed_accuracy:float
     s3_model_path:str 
     trained_model_path:str
+
+
+@dataclass
+class ModelPusherArtifact:
+    bucket_name:str
+    s3_model_path:str
+
+    
