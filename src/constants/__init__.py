4023af9dced593e79a31e708f240037ea62fec70
Nayan Parvez <nayanparvez90@gmail.com>
2025-03-13 02:43:49 -0700

    APP module successfully added with html-template folder and css-static folder and tested on Local-Host

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -71,3 +71,6 @@ MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
 MODEL_BUCKET_NAME = "my-model-mlopsproject"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 5000
